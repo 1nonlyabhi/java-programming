7e871e8a0012ce190607e8b64589905e2470e481
1nonlyabhi <gabhishek0407@gmail.com>
2024-02-24 22:40:06 +0530

    comparator vs comparable

---
+++ java-collections/model/Car.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Car {
+
+    public String carName;
+    public String carType;
+
+    public Car(String carName, String carType) {
+        this.carName = carName;
+        this.carType = carType;
+    }
+}
